2b40705b243700a9873da8886aede810925225cc
Yusuke Tarumi <y.tarumi00@gmail.com>
2013-12-13 07:11:57 +0900

    edit MainActivity.java
    no use xml

--- src/jp/eclipsebook/MainActivity.java
+++ src/jp/eclipsebook/MainActivity.java
@@ -9,16 +9,40 @@ import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
+	private TextView text1;
+	private EditText edit1;
+	private Button btn1;
+	
 	@Override
-	public void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
-	}
-
-	public void doAction(View view){
-		EditText edit1 = (EditText)this.findViewById(R.id.editText1);
-		Editable str = edit1.getText();
-		Toast toast = Toast.makeText(this, "you typed: " + str, Toast.LENGTH_SHORT);
-		toast.show();
+		final Activity activity = this;
+		//ListViewの作成
+		LinearLayout layout = new LinearLayout(this);
+		layout.setOrientation(LinearLayout.VERTICAL);
+		this.setContentView(layout);
+		//TextView作成
+		text1 = new TextView(this);
+		text1.setText("dynamic widget!");
+		text1.setTextSize(30);
+		layout.addView(text1);
+		//EditText作成
+		edit1 = new EditText(this);
+		edit1.setTextSize(30);
+		layout.addView(edit1);
+		//Button作成
+		btn1 = new Button(this);
+		btn1.setText("click");
+		layout.addView(btn1);
+		//Buttonのイベント設定
+		btn1.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Toast toast = Toast.makeText(activity, "you typed: " + edit1.getText(), Toast.LENGTH_SHORT);
+				toast.show();
+			}
+		});
 	}
 }
