a612cd4d4de352a46136ddfac8b2065467923eb1
Yusuke Tarumi <y.tarumi00@gmail.com>
2013-12-13 06:21:51 +0900

    edit MainActivity.java
    test Edit Text

--- src/jp/eclipsebook/MainActivity.java
+++ src/jp/eclipsebook/MainActivity.java
@@ -1,22 +1,23 @@
 package jp.eclipsebook;
 
-import android.os.Bundle;
-
 import android.app.Activity;
-//import android.view.Menu;
-import android.view.View;
-import android.widget.TextView;
+import android.os.Bundle;
+import android.text.Editable;
+import android.view.*;
+import android.widget.*;
 
 public class MainActivity extends Activity {
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 	}
 
 	public void doAction(View view){
+		EditText edit1 = (EditText)this.findViewById(R.id.editText1);
+		Editable str = edit1.getText();
 		TextView text1 = (TextView)this.findViewById(R.id.text1);
-		text1.setText("OK!");
+		text1.setText("you typed: " + str);
 	}
 }
