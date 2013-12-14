1704c26438340e8ec164c18485e2f37402885814
Yusuke Tarumi <y.tarumi00@gmail.com>
2013-12-15 00:18:41 +0900

    edit MainActivity.java
    test radiobutton
    
    Change-Id: Ic9e14f33bfd864333de450fe45a3aeb243b1f9fc

--- src/jp/eclipsebook/MainActivity.java
+++ src/jp/eclipsebook/MainActivity.java
@@ -1,10 +1,11 @@
 package jp.eclipsebook;
 
+/*import android.accounts.Account;
+import android.accounts.OnAccountsUpdateListener;*/
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.*;
-import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.RadioGroup.OnCheckedChangeListener;
 
 public class MainActivity extends Activity {
 
@@ -13,14 +14,16 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 		final Activity activity = this;
-		ToggleButton toggle1 = (ToggleButton)this.findViewById(R.id.toggleButton1);
-		toggle1.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+		RadioGroup group1 = (RadioGroup)this.findViewById(R.id.radioGroup1);
+		group1.setOnCheckedChangeListener(new OnCheckedChangeListener() {
 			
 			@Override
-			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+			public void onCheckedChanged(RadioGroup group, int checkedId) {
 				// TODO Auto-generated method stub
-				Toast toast = Toast.makeText(activity, "you checked: " + isChecked, Toast.LENGTH_SHORT);
+				RadioButton btn = (RadioButton)findViewById(checkedId);
+				Toast toast = Toast.makeText(activity, "you checd: " + btn.getText(), Toast.LENGTH_SHORT);
 				toast.show();
+				toa
 				
 			}
 		});
